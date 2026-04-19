fed927f1db76434ed0835b5fa82e8384df8b86b9
Pavan Patil <pavanpatil8861@gmail.com>
2026-04-19 10:04:59 +0530

    Delete OrderApi directory

--- OrderApi/OrderApi/Repositories/Interfaces/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OrderApi.Models;
-
-namespace OrderApi.Repositories.Interfaces
-{
-    public interface IOrderRepository : IGenericRepository<Order>
-    {
-        bool UpdateStatus(int id, string status);
-    }
-}
